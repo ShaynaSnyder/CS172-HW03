38b8a1230e5d3f8f30d6294040708ff501711654
ssnyder19 <ssnyder19@LAB-SCI-214-05>
2016-09-29 13:00:07 -0700

    In-class thursday initial

---
+++ HW03/QuadraticEquation.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+double getDiscriminant(int a, int b, int c);
+double getRoot1(int a, int b, double discriminant);
+double getRoot2(int a, int b, double discriminant);
+
+class QuadraticEquation
+{
+	int a, b, c;
+	double discriminant;
+	discriminant = getDiscriminant(a, b, c);
+	getRoot1(a, b, discriminant);
+	getRoot2(a, b, discriminant);
+};
+
+double getDiscriminant(int a, int b, int c);
+{
+	int discriminant;
+
+	return discriminant;
+}
+
+double getRoot1(int a, int b, double discriminant)
+{
+	double root1;
+
+}
+
+double getRoot2(int a, int b, double discriminant)
+{
+	double root2;
+
+}
--- HW03/Source.cpp
+++ HW03/Source.cpp
@@ -7,6 +7,7 @@
 #include <iomanip>
 #include <string>
 #include <cstdbool>
+#include "QuadraticEquation.h"
 using namespace std;
 
 class Fan
@@ -16,55 +17,21 @@ public:
 	bool on;
 	//declares double data field that specifies fan radius
 	double radius;
-	//construct a default fan object
+	//constructs a default fan object
 	Fan()
 	{
 		speed = 1;
 		on = false;
 		radius = 5;
 	}
-	//construct a fan object
+	//constructs a fan object
 	Fan(int newSpeed, bool newOn, double newRadius)
 	{
 		speed = newSpeed;
 		on = newOn;
 		radius = newRadius;
 	}
-	//return the properties of the fan
-
 };
-
-double getDiscriminant(int a, int b, int c);
-double getRoot1(int a, int b, double discriminant);
-double getRoot2(int a, int b, double discriminant);
-
-class QuadraticEquation
-{
-	int a, b, c;
-	double discriminant;
-	discriminant = getDiscriminant(a, b, c);
-	getRoot1(a, b, discriminant);
-	getRoot2(a, b, discriminant);
-};
-
-double getDiscriminant(int a, int b, int c);
-{
-	int discriminant;
-
-	return discriminant;
-}
-
-double getRoot1(int a, int b, double discriminant)
-{
-	double root1;
-
-}
-
-double getRoot2(int a, int b, double discriminant)
-{
-	double root2;
-
-}
 
 int main()
 {
@@ -72,6 +39,7 @@ int main()
 	Fan fan1(3, true, 10);
 	Fan fan2(2, false, 5);
 
+	//returns the properties of the fan
 	cout << "Fan 1\nSpeed: " << fan1.speed << "\nRadius: " << fan1.radius << endl;
 	if (fan1.on == 1)
 		cout << "On";
@@ -84,7 +52,12 @@ int main()
 		cout << "Off\n";
 
 	//Exercise 9.6: Algebra: quadratic equations
-
+	cout << "Enter 'a' value: ";
+	cin >> a;
+	cout << "Enter 'b' value: ";
+	cin >> b;
+	cout << "Enter 'c' value: ";
+	cin >> c;
 
 	//Exercise 9.11: The EvenNumber class
 
